2121e21efdf396d45826a9e6ed03fa65d15b599c
rus2lan00 <ruslanovchinnikov520@gmail.com>
2026-05-18 18:49:54 +0500

    Create geography.js

---
+++ data/geography.js
@@ -0,0 +1,108 @@
+window.SUBJECT_GEOGRAPHY = {
+  "subject": "География",
+  "description": "Подготовка к ЕНТ — базовый курс географии, включая физическую географию мира, географию Казахстана и экономическую географию.",
+  "sections": [
+    {
+      "id": "general-geography",
+      "title": "Общая физическая география",
+      "topics": [
+        { "id": "earth-shape", "title": "Форма и размеры Земли" },
+        { "id": "map-projections", "title": "Географические карты и проекции" },
+        { "id": "coordinates", "title": "Географические координаты" },
+        { "id": "earth-rotation", "title": "Движение Земли и часовые пояса" },
+        { "id": "earth-structure", "title": "Внутреннее строение Земли" }
+      ]
+    },
+    {
+      "id": "lithosphere",
+      "title": "Литосфера",
+      "topics": [
+        { "id": "tectonic-plates", "title": "Литосферные плиты и тектоника" },
+        { "id": "minerals-rocks", "title": "Минералы и горные породы" },
+        { "id": "relief", "title": "Рельеф Земли: горы и равнины" },
+        { "id": "volcanism", "title": "Вулканизм и землетрясения" },
+        { "id": "weathering", "title": "Выветривание и эрозия" }
+      ]
+    },
+    {
+      "id": "atmosphere",
+      "title": "Атмосфера и климат",
+      "topics": [
+        { "id": "atmosphere-structure", "title": "Строение атмосферы" },
+        { "id": "weather-climate", "title": "Погода и климат" },
+        { "id": "atmospheric-pressure", "title": "Атмосферное давление и ветры" },
+        { "id": "climate-zones", "title": "Климатические пояса Земли" },
+        { "id": "global-warming", "title": "Глобальное потепление и парниковый эффект" }
+      ]
+    },
+    {
+      "id": "hydrosphere",
+      "title": "Гидросфера",
+      "topics": [
+        { "id": "world-ocean", "title": "Мировой океан" },
+        { "id": "ocean-currents", "title": "Океанические течения" },
+        { "id": "rivers", "title": "Реки и речные системы" },
+        { "id": "lakes-swamps", "title": "Озёра и болота" },
+        { "id": "glaciers", "title": "Ледники и многолетняя мерзлота" },
+        { "id": "groundwater", "title": "Подземные воды" }
+      ]
+    },
+    {
+      "id": "biosphere-geo",
+      "title": "Биосфера и природные зоны",
+      "topics": [
+        { "id": "natural-zones", "title": "Природные зоны Земли" },
+        { "id": "soils", "title": "Почвы и их типы" },
+        { "id": "vegetation-zones", "title": "Растительность и животный мир природных зон" }
+      ]
+    },
+    {
+      "id": "continents",
+      "title": "Материки и океаны",
+      "topics": [
+        { "id": "eurasia", "title": "Евразия" },
+        { "id": "africa", "title": "Африка" },
+        { "id": "north-america", "title": "Северная Америка" },
+        { "id": "south-america", "title": "Южная Америка" },
+        { "id": "australia", "title": "Австралия и Океания" },
+        { "id": "antarctica", "title": "Антарктида" },
+        { "id": "oceans", "title": "Океаны Земли" }
+      ]
+    },
+    {
+      "id": "kazakhstan-physical",
+      "title": "Физическая география Казахстана",
+      "topics": [
+        { "id": "kz-position", "title": "Географическое положение Казахстана" },
+        { "id": "kz-relief", "title": "Рельеф Казахстана" },
+        { "id": "kz-minerals", "title": "Полезные ископаемые Казахстана" },
+        { "id": "kz-climate", "title": "Климат Казахстана" },
+        { "id": "kz-rivers-lakes", "title": "Реки и озёра Казахстана" },
+        { "id": "kz-natural-zones", "title": "Природные зоны Казахстана" }
+      ]
+    },
+    {
+      "id": "kazakhstan-economic",
+      "title": "Экономическая и социальная география Казахстана",
+      "topics": [
+        { "id": "kz-population", "title": "Население Казахстана" },
+        { "id": "kz-industry", "title": "Промышленность Казахстана" },
+        { "id": "kz-agriculture", "title": "Сельское хозяйство Казахстана" },
+        { "id": "kz-transport", "title": "Транспорт Казахстана" },
+        { "id": "kz-regions", "title": "Экономические районы Казахстана" }
+      ]
+    },
+    {
+      "id": "world-economic",
+      "title": "Экономическая география мира",
+      "topics": [
+        { "id": "world-population", "title": "Население мира" },
+        { "id": "world-urbanization", "title": "Урбанизация и города мира" },
+        { "id": "world-resources", "title": "Природные ресурсы мира" },
+        { "id": "world-industry", "title": "Промышленность мира" },
+        { "id": "world-agriculture", "title": "Сельское хозяйство мира" },
+        { "id": "world-globalization", "title": "Международные организации и глобализация" }
+      ]
+    }
+  ]
+};
