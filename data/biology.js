bc96534930c75f811c9234c33f2dcd0a1e39f6c7
rus2lan00 <ruslanovchinnikov520@gmail.com>
2026-05-18 18:43:44 +0500

    Create biology.js

---
+++ data/biology.js
@@ -0,0 +1,96 @@
+ent-tutor / data/biology.jswindow.SUBJECT_BIOLOGY = {
+  "subject": "Биология",
+  "description": "Подготовка к ЕНТ — базовый курс биологии по разделам школьной программы Казахстана.",
+  "sections": [
+    {
+      "id": "cell-biology",
+      "title": "Цитология (клетка)",
+      "topics": [
+        { "id": "cell-structure", "title": "Строение клетки" },
+        { "id": "prokaryotes-eukaryotes", "title": "Прокариоты и эукариоты" },
+        { "id": "cell-organelles", "title": "Органоиды клетки и их функции" },
+        { "id": "cell-membrane", "title": "Клеточная мембрана и транспорт веществ" },
+        { "id": "cell-division", "title": "Деление клетки: митоз и мейоз" }
+      ]
+    },
+    {
+      "id": "molecular-biology",
+      "title": "Молекулярная биология",
+      "topics": [
+        { "id": "proteins", "title": "Белки: строение и функции" },
+        { "id": "nucleic-acids", "title": "Нуклеиновые кислоты: ДНК и РНК" },
+        { "id": "carbohydrates-lipids", "title": "Углеводы и липиды" },
+        { "id": "protein-synthesis", "title": "Биосинтез белка (транскрипция, трансляция)" },
+        { "id": "atp-energy", "title": "АТФ и энергетический обмен" }
+      ]
+    },
+    {
+      "id": "genetics",
+      "title": "Генетика",
+      "topics": [
+        { "id": "mendel-laws", "title": "Законы Менделя" },
+        { "id": "chromosome-theory", "title": "Хромосомная теория наследственности" },
+        { "id": "sex-genetics", "title": "Генетика пола и сцепленное с полом наследование" },
+        { "id": "mutations", "title": "Мутации и их виды" },
+        { "id": "genetic-problems", "title": "Решение генетических задач" }
+      ]
+    },
+    {
+      "id": "botany",
+      "title": "Ботаника (растения)",
+      "topics": [
+        { "id": "plant-tissues", "title": "Ткани растений" },
+        { "id": "plant-organs", "title": "Органы растения: корень, стебель, лист" },
+        { "id": "photosynthesis", "title": "Фотосинтез" },
+        { "id": "plant-reproduction", "title": "Размножение растений" },
+        { "id": "plant-classification", "title": "Классификация растений" }
+      ]
+    },
+    {
+      "id": "zoology",
+      "title": "Зоология (животные)",
+      "topics": [
+        { "id": "invertebrates", "title": "Беспозвоночные животные" },
+        { "id": "arthropods", "title": "Членистоногие" },
+        { "id": "fish-amphibians", "title": "Рыбы и земноводные" },
+        { "id": "reptiles-birds", "title": "Пресмыкающиеся и птицы" },
+        { "id": "mammals", "title": "Млекопитающие" }
+      ]
+    },
+    {
+      "id": "human-anatomy",
+      "title": "Анатомия и физиология человека",
+      "topics": [
+        { "id": "skeletal-system", "title": "Опорно-двигательная система" },
+        { "id": "circulatory-system", "title": "Кровь и кровообращение" },
+        { "id": "respiratory-system", "title": "Дыхательная система" },
+        { "id": "digestive-system", "title": "Пищеварительная система" },
+        { "id": "excretory-system", "title": "Выделительная система" },
+        { "id": "nervous-system", "title": "Нервная система" },
+        { "id": "endocrine-system", "title": "Эндокринная система" },
+        { "id": "sense-organs", "title": "Органы чувств" },
+        { "id": "reproductive-system", "title": "Размножение и развитие человека" }
+      ]
+    },
+    {
+      "id": "evolution",
+      "title": "Эволюционное учение",
+      "topics": [
+        { "id": "darwin-theory", "title": "Теория Дарвина" },
+        { "id": "evolution-evidence", "title": "Доказательства эволюции" },
+        { "id": "species-speciation", "title": "Вид и видообразование" },
+        { "id": "human-evolution", "title": "Происхождение человека" }
+      ]
+    },
+    {
+      "id": "ecology",
+      "title": "Экология",
+      "topics": [
+        { "id": "ecosystems", "title": "Экосистемы и биогеоценозы" },
+        { "id": "food-chains", "title": "Пищевые цепи и сети" },
+        { "id": "biosphere", "title": "Биосфера и её охрана" },
+        { "id": "population", "title": "Популяции и их свойства" }
+      ]
+    }
+  ]
+};
